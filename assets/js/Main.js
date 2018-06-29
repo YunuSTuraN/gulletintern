d93046a982e46ddaa8db873a350607e40a8c86e6
Yunus Turan Yılmaz <yunus.yilmaz@orgino.com.tr>
2018-06-29 18:48:13 +0300

    added slider

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -33,3 +33,43 @@ $('.myslider').slick({
         // instead of a settings object
     ]
 });
+$('.big_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.small_slider'
+});
+$('.small_slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.big_slider',
+    dots: true,
+    centerMode: false,
+    focusOnSelect: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
